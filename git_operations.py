ef7e2aefc999a58ebc29528b77bfdba07d73a4b8
hagamesai <hagame1988@163.com>
2024-11-05 09:38:27 +0800

    Add code: git_operations.py

--- git_operations.py
+++ git_operations.py
@@ -8,29 +8,26 @@ class GitOperations:
 
     def __init__(self, repo_path):
         """
-        Initializes the GitOperations with the repository path.
-        :param repo_path: Path to the git repository.
+        Initializes the GitOperations with the path to the repository.
         """
         self.repo_path = repo_path
 
     def clone_repo(self, repo_url):
         """
-        Clones a git repository from a given URL.
-        :param repo_url: The URL of the repository to clone.
+        Clones the repository from the given URL.
         """
         subprocess.run(['git', 'clone', repo_url], cwd=self.repo_path)
 
     def commit_changes(self, message):
         """
-        Commits changes in the repository with a commit message.
-        :param message: The commit message.
+        Commits changes in the repository with the provided message.
         """
         subprocess.run(['git', 'add', '.'], cwd=self.repo_path)
         subprocess.run(['git', 'commit', '-m', message], cwd=self.repo_path)
 
     def push_changes(self):
         """
-        Pushes changes to the remote repository.
+        Pushes committed changes to the remote repository.
         """
         subprocess.run(['git', 'push'], cwd=self.repo_path)
 
