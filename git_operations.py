c171cc19c06a63cf0fde0dbfdc87c77035adced3
hagamesai <hagame1988@163.com>
2024-11-12 05:38:51 +0800

    Add code: git_operations.py

--- git_operations.py
+++ git_operations.py
@@ -8,38 +8,43 @@ class GitOperations:
 
     def __init__(self, repo_path):
         """
-        Initialize with the path to the Git repository.
+        Initializes the GitOperations with the repository path.
+        :param repo_path: Path to the Git repository.
         """
         self.repo_path = repo_path
 
-    def clone_repo(self, repo_url):
+    def run_command(self, command):
         """
-        Clone a Git repository from the given URL.
+        Runs a shell command in the context of the repository.
+        :param command: Command to run.
+        :return: Output of the command.
         """
-        subprocess.run(['git', 'clone', repo_url], cwd=self.repo_path)
-
-    def commit_changes(self, message):
-        """
-        Commit changes in the repository with a provided message.
-        """
-        subprocess.run(['git', 'add', '.'], cwd=self.repo_path)
-        subprocess.run(['git', 'commit', '-m', message], cwd=self.repo_path)
-
-    def push_changes(self):
-        """
-        Push committed changes to the remote repository.
-        """
-        subprocess.run(['git', 'push'], cwd=self.repo_path)
-
-    def pull_changes(self):
-        """
-        Pull changes from the remote repository.
-        """
-        subprocess.run(['git', 'pull'], cwd=self.repo_path)
-
-    def get_status(self):
-        """
-        Get the current status of the repository.
-        """
-        result = subprocess.run(['git', 'status'], cwd=self.repo_path, capture_output=True, text=True)
+        result = subprocess.run(command, shell=True, cwd=self.repo_path,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                text=True)
+        if result.returncode != 0:
+            raise Exception(f'Error: {result.stderr}')
         return result.stdout
+
+    def clone(self, repo_url):
+        """
+        Clones a repository from a given URL.
+        :param repo_url: URL of the repository to clone.
+        """
+        command = f'git clone {repo_url}'
+        return self.run_command(command)
+
+    def pull(self):
+        """
+        Pulls the latest changes from the remote repository.
+        """
+        command = 'git pull'
+        return self.run_command(command)
+
+    def status(self):
+        """
+        Returns the status of the repository.
+        :return: Status of the repository.
+        """
+        command = 'git status'
+        return self.run_command(command)
