4c59f47abaef1d4f0fce7974dd63fbb559fc2753
hagamesai <hagame1988@163.com>
2025-01-16 00:45:36 +0800

    feat: implement CRUD operations for data management

--- src/api/data.js
+++ src/api/data.js
@@ -2,44 +2,44 @@ const express = require('express');
 const router = express.Router();
 const DataModel = require('../models/dataModel');
 
-// Create new data
+// Create Data
 router.post('/', async (req, res) => {
     try {
         const newData = new DataModel(req.body);
         const savedData = await newData.save();
         res.status(201).json(savedData);
-    } catch (err) {
-        res.status(500).json(err);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
-// Read all data
+// Read Data
 router.get('/', async (req, res) => {
     try {
         const data = await DataModel.find();
         res.status(200).json(data);
-    } catch (err) {
-        res.status(500).json(err);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
-// Update data
+// Update Data
 router.put('/:id', async (req, res) => {
     try {
         const updatedData = await DataModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.status(200).json(updatedData);
-    } catch (err) {
-        res.status(500).json(err);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
-// Delete data
+// Delete Data
 router.delete('/:id', async (req, res) => {
     try {
         await DataModel.findByIdAndDelete(req.params.id);
-        res.status(204).send();
-    } catch (err) {
-        res.status(500).json(err);
+        res.status(204).json();
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
