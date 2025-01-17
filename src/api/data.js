514aca10d1f2260bb6ae0d7bab674a8a4371befa
hagamesai <hagame1988@163.com>
2025-01-17 08:52:15 +0800

    feat: implement data management API

--- src/api/data.js
+++ src/api/data.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 const DataModel = require('../models/dataModel');
 
-// Create Data
+// Create a new data entry
 router.post('/', async (req, res) => {
     try {
         const newData = new DataModel(req.body);
-        const savedData = await newData.save();
-        res.status(201).json(savedData);
+        await newData.save();
+        res.status(201).json(newData);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
 });
 
-// Read Data
+// Get all data entries
 router.get('/', async (req, res) => {
     try {
         const data = await DataModel.find();
@@ -23,21 +23,21 @@ router.get('/', async (req, res) => {
     }
 });
 
-// Update Data
+// Update a data entry
 router.put('/:id', async (req, res) => {
     try {
         const updatedData = await DataModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.status(200).json(updatedData);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
 });
 
-// Delete Data
+// Delete a data entry
 router.delete('/:id', async (req, res) => {
     try {
         await DataModel.findByIdAndDelete(req.params.id);
-        res.status(204).json();
+        res.status(204).send();
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
